b6f8b0607e6c08d8539058ffccad0712f58ae956
itechpaulj <itechpaulj@gmail.com>
2024-08-16 01:21:08 +0800

    initial commit

---
+++ wp-content/plugins/tablesome/vendor/composer/installed.php
@@ -0,0 +1,55 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pauple/tablesome',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'c8e27415bace15ae9d7e8b7c55f271ce8ba43736',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pauple/pluginator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '742ec88b48048ebbe4a5fab23b0560f715bd473c',
+            'type' => 'pauple-library',
+            'install_path' => __DIR__ . '/../pauple/pluginator',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'pauple/tablesome' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'c8e27415bace15ae9d7e8b7c55f271ce8ba43736',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
